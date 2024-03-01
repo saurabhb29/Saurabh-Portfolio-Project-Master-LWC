a0f91288c2ff31031a47273af35b7d3bab68ce23
Saurabh Bisht <1999saurabhbisht@gmail.com>
2024-03-01 17:28:08 +0530

    new v

---
+++ force-app/main/default/lwc/portfolioBanner/portfolioBanner.js
@@ -0,0 +1,44 @@
+import { LightningElement, wire } from 'lwc';
+import PortfolioAssets from '@salesforce/resourceUrl/PortfolioAssets'
+import {getRecord,getFieldValue} from 'lightning/uiRecordApi';
+import FULL_NAME from '@salesforce/schema/Portfolio__c.FullName__c';
+import DESIGNATION from '@salesforce/schema/Portfolio__c.Designation__c';
+import COMPANY_NAME from '@salesforce/schema/Portfolio__c.CompanyName__c';
+import COMPANY_LOCATION from '@salesforce/schema/Portfolio__c.CompanyLocation__c';
+export default class PortfolioBanner extends LightningElement {
+    linkedinUrl = 'https://www.linkedin.com/in/saurabh-bisht-953816174/'
+    githubUrl = 'https://github.com/saurabhb29'
+    youtubeUrl = 'https://www.youtube.com/@SaurabhBishtVlogs-li7qr'
+    trailheadUrl = 'https://www.salesforce.com/trailblazer/sbdeveloper'
+
+    userPic = `${PortfolioAssets}/PortfolioAssets/SaurabhBisht.png`
+    linkedin = `${PortfolioAssets}/PortfolioAssets/Social/linkedin.svg`
+    youtube = `${PortfolioAssets}/PortfolioAssets/Social/youtube.svg`
+    github = `${PortfolioAssets}/PortfolioAssets/Social/github.svg`
+    trailhead = `${PortfolioAssets}/PortfolioAssets/Social/trailhead1.svg`
+
+    recordId='a00GC00004kJUv5YAG';
+    @wire(getRecord,{recordId:'$recordId', fields:[FULL_NAME,COMPANY_LOCATION,COMPANY_NAME,DESIGNATION]})
+    portfolioData
+    // portfolioHandler({data, error}){
+    //     if(data){
+    //         console.log("record Data", JSON.stringify(data))
+    //     }
+    //     if(error){
+    //         console.error("error", error)
+    //     }
+    // }
+
+    get fullName(){
+        return getFieldValue(this.portfolioData.data, FULLNAME)
+    }
+    get companyLocation(){
+        return getFieldValue(this.portfolioData.data, COMPANY_LOCATION)
+    }
+    get companyName(){
+        return getFieldValue(this.portfolioData.data, COMPANY_NAME)
+    }
+    get designation(){
+        return getFieldValue(this.portfolioData.data, DESIGNATION)
+    }
+  }
